3b9c9ca6ec38a8ee90c43acd321dfd8b75fca4a3
Ganiyu Ayomide <ganiyuaa2019@gmail.com>
2022-09-26 21:35:22 +0000

    task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,8 +4,8 @@ def replace_in_list(my_list, idx, elem):
     """ 
     replace an elment from a list at index idx with elem
     Args:
-    my_list - list to search
-    idx - the position to access
+        my_list - list to search
+        idx - the position to access
         elem - new elem to swap with
     Return:
         my_list
